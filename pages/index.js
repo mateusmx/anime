2378e34c9328128c06895dbff8084884f0734bd1
Pablo Díaz Mateu <pablodiazmateus@gmail.com>
2021-11-29 16:15:32 -0600

    Adding axios test with the API endpoint.

--- pages/index.js
+++ pages/index.js
@@ -2,15 +2,30 @@ import Head from 'next/head'
 import { useEffect } from 'react'
 import { useSelector, useDispatch } from 'react-redux'
 import { setMovies } from '../redux/actions/main'
+import ax from '../axiosConfig';
 
 export default function Home() {
 
   const dispatch = useDispatch();
   const movies = useSelector(state => state.main.movies)
-  console.log(movies);
 
   useEffect(() => {
-    dispatch(setMovies('test'));
+    getData();
+    async function getData() {
+      const response = await ax.get('/anime');
+      const animesArray = [];
+      response.data.data.map((data) => {
+        let anime = {};
+        anime.title = data.attributes.titles.en;
+        anime.img = data.attributes.posterImage.medium;
+        anime.rating = data.attributes.averageRating
+        anime.favorites = data.attributes.favoritesCount
+
+        animesArray.push(anime);
+      })
+      dispatch(setMovies(animesArray));
+    }
+
   }, [])
 
   return (
@@ -21,7 +36,7 @@ export default function Home() {
         <link rel="icon" href="https://www.cruisebound.com/favicon.ico" />
       </Head>
       <div>
-        En casa {movies}
+        En casa
       </div>
     </>
   )
